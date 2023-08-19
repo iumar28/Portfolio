1dc91bc8a34e6a328bc79c4998328729ca4574e2
iumar28 <umar9897222@gmail.com>
2023-08-19 15:24:19 +0530

    fifth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,29 +23,28 @@ const Hero = () => {
       </div>
       <section
         className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
-        sm:bg-hero bg-hero-mobile overflow-hidden"
-        style={{ color: '#6CE5E8' }}>
+        sm:bg-hero bg-hero-mobile overflow-hidden">
         <div
           className={`absolute inset-0 sm:top-[250px] top-[150px] 
           lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
           max-w-7xl mx-auto flex flex-row items-start
           justify-between gap-3`}>
+          <div className="flex flex-col justify-center items-center mt-5 ml-3">
+            <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden" />
+            <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden" />
+          </div>
 
           <div>
             <h1
-              className={`${styles.heroHeadText} font-poppins uppercase`}>
+              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
               Hi, I'm{' '}
               <span
                 className="sm:text-battleGray sm:text-[90px] 
-                text-[#6CE5E8] text-[50px] font-mova
+                text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
                 Umar Firoz
               </span>
-            </h1>
-            <p className={`${styles.heroSubText} mt-2`}>
-              Full Stack Web Developer <br className="sm:block hidden" />
-              
-            </p>
+              </h1>           
           </div>
 
           <div
